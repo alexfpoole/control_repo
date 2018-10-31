084dee234086d62edb4acaf9a62c6cac6408cf69
gingabeard <38758494+gingabeard@users.noreply.github.com>
2018-10-31 10:27:02 +0000

    Create init.pp

---
+++ site/mine/manifests/init.pp
@@ -0,0 +1,21 @@
+class mine {
+  file {'/opt/mine':
+    ensure => directory,
+  }
+  file {'/opt/mine/mineserver.jar':
+    ensure => file
+    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  }
+  package {'java':
+    ensure => present,
+  }
+  file {'/opt/mine/eula.txt':
+    ensure => file,
+    content => 'eula=true',
+  }
+  file {'/etc/systemd/system/minecraft.service':
+    ensure => file,
+    source => 'puppet:///modules/mine/minecraft.service',
+  }
+}
+    
